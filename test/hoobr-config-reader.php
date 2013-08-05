af3e594b668ac1fbe27041327b763c9aee03e3a3
Ric Allinson <capecodehq@yahoo.co.uk>
2013-08-04 20:59:29 -0700

    Updated tests

--- test/hoobr-config-reader.php
+++ test/hoobr-config-reader.php
@@ -9,16 +9,42 @@ ini_set('display_errors', 'on');
 $module = new stdClass();
 require(__DIR__ . "/../index.php");
 $reader = $module->exports;
+
 /*
     Now we test it.
 */
 
 describe("hoobr-config-reader", function () use ($reader) {
 
-    it("should return ", function () use ($reader) {
+    it("should return [value2]", function () use ($reader) {
 
         $config = $reader("sample", "./fixtures", "./fixtures/node_modules");
 
         assert($config->get("key2") === "value2");
+    });
+
+    it("should return [val1]", function () use ($reader) {
+
+        $config = $reader("sample", "./fixtures", "./fixtures/node_modules");
+
+        $array = $config->get();
+
+        assert($array["key1"] === "val1");
+    });
+
+    it("should return [true]", function () use ($reader) {
+
+        $config = $reader("sample", "./fixtures", "./fixtures/node_modules");
+
+        assert($config->put("key3", "value3") === true);
+        assert($config->get("key3") === "value3");
+    });
+
+    it("should return [false]", function () use ($reader) {
+
+        $config = $reader("sample", "./fixtures", "./fixtures/node_modules");
+
+        assert($config->put("key4", "val4") === false);
+        assert($config->get("key4") === null);
     });
 });
